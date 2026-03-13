c227acc90a4dd338be03edb6126fbb83dd13c0d0
Muhammad Zulqarnain <rajazulqarnain2407@gmail.com>
2026-03-13 16:22:28 +0500

    add filtering option

--- modules/courses.py
+++ modules/courses.py
@@ -1,3 +1,4 @@
+import json
 
 
 def getCourses(service):
@@ -7,9 +8,35 @@ def getCourses(service):
 
     courses=results.get('courses',[])
 
-    for course in courses:
-        print(course['name'],"| Section:",course.get('section','N/A',),"| ID:",course['id'])
-
     return courses
     
 
+def loadRelevantCourses():
+    with open('data/relevant_courses.json',"r") as f:
+        data=json.load(f)
+
+
+
+    return data["courses"]
+
+
+def filterCourses(courses):
+
+    relevant_names=loadRelevantCourses()
+
+    filtered=[]
+
+    for course in courses:
+
+        if course["name"] in relevant_names:
+            filtered.append(course)
+
+    return filtered
+
+
+def printCourses(courses):
+
+    for course in courses:
+        print(course['name'],"| Section:",course.get('section','N/A',),"| ID:",course['id'])
+
+        
